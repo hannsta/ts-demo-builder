007458b5b74ec8e18c48641926607712be3d7583
hannsta <schroeder.nt@gmail.com>
2024-07-09 10:53:09 -0400

    improve add a viz window

--- src/Views/Popups/VizSelector.tsx
+++ src/Views/Popups/VizSelector.tsx
@@ -1,6 +1,9 @@
 import { useContext, useEffect, useState } from "react"
 import { createClientWithoutAuth } from "../../Util/Util"
 import { SettingsContext } from "../../App"
+import { CloseButton } from "../../Settings/Inputs/InputMenus"
+import { HiTableCells } from "react-icons/hi2"
+import { FaChartArea, FaChartBar, FaChartLine, FaChartPie, FaTable } from "react-icons/fa";
 
 interface VizSelectorProps {
     liveboardId: string,
@@ -10,10 +13,12 @@ interface VizSelectorProps {
 export interface Viz {
     id: string,
     name: string,
+    chartType: string,
     tml: any,
 }
 const VizSelector: React.FC<VizSelectorProps> = ({liveboardId, setVizId, setVizSelectorVisible}) => {
     const [vizList, setVizList] = useState<Viz[]>([])
+    const [vizListLoaded, setVizListLoaded] = useState<boolean>(false)
     const settingsContext = useContext(SettingsContext);
     useEffect(() => {
         let client =  createClientWithoutAuth(settingsContext.settings.TSURL);
@@ -34,26 +39,60 @@ const VizSelector: React.FC<VizSelectorProps> = ({liveboardId, setVizId, setVizS
                 vizList.push({
                     id: viz.viz_guid,
                     name: viz.answer.name,
+                    chartType: viz.answer.chart.type,
                     tml: viz
                 })
             })
             setVizList(vizList)
+            setVizListLoaded(true)
         })
     }, [])
 
     return (
-        <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center' style={{zIndex:999}}>
-            <div className='absolute flex flex-col bg-white rounded-lg p-8 w-96 shadow-2xl'>
-                <div className="font-bold text-xl mb-4">Select a Viz</div>
-                <div className="flex flex-col space-y-2">
-                    {vizList.map((viz, index) => (
-                        <div key={index} className="flex flex-row items-center justify-between hover:cursor-pointer hover:text-blue-400" onClick={() => setVizId(viz)}>
-                            {viz.name}
-                        </div>
-                    ))}
+        <div className='absolute top-0 left-0 flex justify-center items-center overflow-hidden' style={{zIndex:999, width:'calc(100vh - 19rem)', height:'calc(100vh - 8rem)'}}>
+            <div className='absolute flex flex-col bg-white rounded-lg p-4 shadow-2xl overflow-auto' style={{ maxHeight:'800px', minWidth:'400px', minHeight:'400px'}}>
+                <div className="flex p-4 flex-row justify-between items-center"> 
+                    <div className="font-bold text-xl mb-4">Select a Viz</div>
+                    <CloseButton onClick={()=>setVizSelectorVisible(false)}/>
                 </div>
-                <div className="flex flex-row justify-end">
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setVizSelectorVisible(false)}>Close</button>
+                <div className="flex overflow-auto p-4" style={{ maxHeight:'800px', minWidth:'400px', minHeight:'400px',scrollbarWidth:'thin'}}>
+                    {vizList.length > 0 && vizListLoaded && (
+                        <div className="grid grid-cols-3 gap-4" >
+                            {vizList.map((viz, index) => (
+                                <div key={index} className="flex flex-col hover:cursor-pointer hover:font-bold hover:shadow-xl h-44 w-44 overflow-hidden shadow-md rounded-md" onClick={() => setVizId(viz)}>
+                                    <div className="bg-gray-100 p-2" >
+                                        {viz.chartType == 'PIVOT_TABLE' && (
+                                            <FaTable />
+                                        )}
+                                        {viz.chartType == 'LINE' && (
+                                            <FaChartLine />
+                                        )}
+                                        {viz.chartType == 'BAR' && (
+                                            <FaChartBar />
+                                        )}
+                                        {viz.chartType == 'AREA' || viz.chartType == 'KPI' && (
+                                            <FaChartArea />
+                                        )}
+                                        {viz.chartType == 'PIE' && (
+                                            <FaChartPie />
+                                        )}
+                                        {['PIVOT_TABLE', 'LINE', 'BAR', 'AREA', 'KPI', 'PIE'].indexOf(viz.chartType) == -1 && (
+                                            <FaTable />
+                                        )}
+                                    </div>
+                                    <div className="p-4">
+                                    {viz.name}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                    {vizList.length == 0 && vizListLoaded && (
+                        <div className="flex h-full w-full align-center items-center justify-center text-md">No Vizs Found</div>
+                    )}
+                    {vizList.length == 0 && !vizListLoaded && (
+                        <div className="flex h-full w-full align-center items-center justify-center text-md">Loading Viz List ...</div>
+                    )}
                 </div>
             </div>
         </div>
