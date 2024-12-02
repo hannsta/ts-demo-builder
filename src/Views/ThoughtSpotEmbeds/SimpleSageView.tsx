f3ddc66a8965e38197a4d9db0af4615ace3fab8e
Nathan Schroeder <nathan.schroeder@thoughtspot.com>
2024-12-02 08:47:34 -0500

    updates

--- src/Views/ThoughtSpotEmbeds/SimpleSageView.tsx
+++ src/Views/ThoughtSpotEmbeds/SimpleSageView.tsx
@@ -1,15 +1,41 @@
+import { ConversationEmbed, EmbedEvent } from "@thoughtspot/visual-embed-sdk";
 import { SageEmbed } from "@thoughtspot/visual-embed-sdk/react";
+import { useEffect } from "react";
 import { SimpleSage } from "../../Settings/StandardMenus/SimpleSageConfig";
 
 interface SimpleSageProps {
     simpleSage: SimpleSage,
 }
 const SimpleSageView: React.FC<SimpleSageProps> = ({simpleSage}) => {
+    let embed;
+    useEffect(()=>{
+        let div = document.getElementById("conversation");
+        if (div){
+            embed = new ConversationEmbed(div,{
+                worksheetId: simpleSage.worksheet,
+                frameParams: {
+                    width:'100%',
+                    height:'800px'
+                },
+                customizations:{
+                    style:{
+                        customCSS:{
+                            rules_UNSTABLE:{
+                                '.zauthenticated-app-view-module__pageContent':{overflow:'hidden'}
+                            }
+                        }
+                    }
+                }
+            })
+            embed.render();
+            embed.on(EmbedEvent.ALL, (data: any)=>{
+                console.log(data)
+            })
+        }
+
+    },[])
     return (
-        <SageEmbed
-            frameParams={{width: '100%', height: '100%'}}
-            dataSource={simpleSage && simpleSage.worksheet ? simpleSage.worksheet : ''}
-        />
+        <div id="conversation"></div>
     )
 }
 export default SimpleSageView;
